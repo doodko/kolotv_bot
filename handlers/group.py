c0acb47eb1b5234e99bea260da9da94404065137
Oleksandr Dudko <alexdoodko@gmail.com>
2023-06-30 18:35:55 +0300

    Add mention service to group handlers

--- handlers/group.py
+++ handlers/group.py
@@ -1,21 +1,27 @@
+import re
+
 from aiogram import Router, F
 from aiogram.types import Message, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 from bot import bot
 from config_reader import config
-
+from database.mention_service import MentionService
+from database.word_service import WordService
 
 router = Router()
 
-alert_list = r".*(коло|[иі]нтернет|провайдер).*"
+ws = WordService()
+ms = MentionService()
 
 
-@router.message(F.text.lower().regexp(alert_list))
+@router.message(F.text.lower().regexp(config.pattern))
 async def ping_kivi(message: Message):
+
+    ms.add_new_mention(message)
+
     builder = InlineKeyboardBuilder()
     builder.row(InlineKeyboardButton(text="Jump to message", url=make_msg_link(message)))
-
     await bot.send_message(chat_id=config.superuser_id, text=make_msg(message), reply_markup=builder.as_markup())
 
 
@@ -27,7 +33,7 @@ def make_msg(message: Message) -> str:
 
 
 def make_msg_link(message: Message) -> str:
-    chat_id = message.chat.username or (message.chat.id + 1000000000000) * -1
+    chat_id = (message.chat.id + 1000000000000) * -1
     message_id = message.message_id
     link = f"https://t.me/c/{chat_id}/{message_id}"
 
