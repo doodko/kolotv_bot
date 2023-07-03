160e5897df6cb639f6459d4dc0ffa13009381e1c
doodko <77903448+doodko@users.noreply.github.com>
2023-07-03 12:22:20 +0300

    Develop (#4)
    * Add python-dotenv to requirements.txt
    * Add config parser file
    * Move credentials to .env
    * Separate message handlers
    * Add link to message button
    * Add models.py for sqlalchemy
    * Add the db folder and move the files that belong to it
    * Add db to config_reader
    * Add main pattern to config parser
    * Move db.py to database folder
    * Add mention service to group handlers
    * Add mention service
    * Remove chat from models
    * Change echo function to stats command
    * Add utils
    * Add words service
    * Add notice message when bot started
    * Change mention message receiver to private channel
    * Add stats count function
    * Add lost stats

--- handlers/private.py
+++ handlers/private.py
@@ -2,9 +2,13 @@ from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message
 
+from database.utils import Utils
+
 
 router = Router()
 router.message.filter(F.chat.type =='private')
+
+ut = Utils()
 
 
 @router.message(Command(commands=["start"]))
@@ -14,4 +18,4 @@ async def cmd_start(message: Message) -> None:
 
 @router.message(Command(commands=["stats"]))
 async def cmd_stats(message: Message):
-    await message.answer(text='here we go!')
+    await message.answer(text=ut.give_statistics())
