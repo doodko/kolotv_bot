ae5baee0954f2cca3b28a844bafb3ee3b4fc5acb
Oleksandr Dudko <alexdoodko@gmail.com>
2023-09-13 11:12:26 +0300

    Remove seed_db func

--- services/word_service.py
+++ services/word_service.py
@@ -14,21 +14,5 @@ class WordService:
     def get_all_words(self) -> list[Word]:
         return self.session.query(Word).all()
 
-    def seed_db(self):
-        words_list = [('коло', '\\bколо\\b'),
-                      ('провайдер', '\\bпровайдер'),
-                      ('інтернет', '\\b[іи]нтернет'),
-                      ('локалнет', '\\bлокалн[еэє]т\\b'),
-                      ('павутина', '\\bпав?утин'),
-                      ('wi-fi', '\\b(wi\\-?fi|вай\\-?фай)\\b'),
-                      ('утелс', '\\b(utels|утелс)\\b'),
-                      ('вега', '\\b(vega|вега)\\b'),
-                      ('київстар', '\\b(ки[їєе]встар|kyivstar)'),
-                      ('роутер', '\\bроутер')]
-
-        for word in words_list:
-            name, pattern = word
-            self.add_new_word(name=name, pattern=pattern)
-
 
 word_service = WordService()
